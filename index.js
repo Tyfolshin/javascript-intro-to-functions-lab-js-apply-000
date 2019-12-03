08f9d7ebf8a943fac685d9bba2f7b08804d6894e
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2019-12-03 23:41:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ function sayHiToGrandma(string){
   if(string.toLowerCase() === string){
     return "I can't hear you!"
   }
+<<<<<<< HEAD
   if("I love you, Grandma."=== string){
+=======
+  if(string === "I love you, Grandma"){
+>>>>>>> 51e8b0da59910b903b915671155a74f3b11e8e27
     return "I love you, too."
   }
 }
